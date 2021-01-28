346f1370dd641598f9db2d2dbfa88891a83b7acc
kamiroudj <39601833+kamiroudj@users.noreply.github.com>
2021-01-28 14:26:56 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("******************Hello world et Welcom to jenkins training*****************");
+		System.out.println("******************test changes*****************");
 
 	}
 
